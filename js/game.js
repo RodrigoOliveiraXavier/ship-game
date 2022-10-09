6f727e0b0176b764bea102a89ae716bec83a8354
RodrigoOliveiraXavier <rodrigo.oliveira.xavier151@gmail.com>
2022-10-09 07:05:10 -0300

    [OK] Criado validação para caso o projetil esteja fora da tela

--- js/game.js
+++ js/game.js
@@ -10,8 +10,8 @@ let prevTime = 0;
 
 const player = new Player({
   position: {
-    x: cnv.width/2,
-    y: cnv.height/2
+    x: cnv.width / 2,
+    y: cnv.height / 2
   },
   radius: 30,
   color: '#48FCFF'
@@ -26,7 +26,7 @@ cnv.addEventListener('click', (e) => {
   const angle = Math.atan2(e.clientY - player.position.y, e.clientX - player.position.x);
   const velocity = {
     x: Math.cos(angle) * shootingSpeed,
-    y: Math.sin(angle) * shootingSpeed 
+    y: Math.sin(angle) * shootingSpeed
   };
 
   projectiles.push(new Projectile({
@@ -43,27 +43,39 @@ cnv.addEventListener('click', (e) => {
 function animate() {
   window.requestAnimationFrame(animate, cnv);
   update();
-  
+
   //Calcula FPS
   let delta = (performance.now() - prevTime) / 1000;
   let fps = 1 / delta;
-  
+
   prevTime = performance.now();
   // console.log(`FPS: ${fps}`)
 }
 
 function update() {
   ctx.fillStyle = 'rgba(0, 0, 0, .1)';
-  ctx.fillRect(0,0,cnv.width, cnv.height);
-  
+  ctx.fillRect(0, 0, cnv.width, cnv.height);
+
   checkProjectiles();
   player.update();
 }
 
 function checkProjectiles() {
-  for(let i = projectiles.length -1; i >= 0; i--) {
+  for (let i = projectiles.length - 1; i >= 0; i--) {
     const p = projectiles[i];
     p.update();
+
+    checkOffScreen(p, i);
+  }
+}
+
+function checkOffScreen(projectile, index) {
+  if (projectile.position.x + projectile.radius < 0 ||
+    projectile.position.x - projectile.radius > cnv.width ||
+    projectile.position.y + projectile.radius < 0 ||
+    projectile.position.y - projectile.radius > cnv.height) {
+
+    projectiles.splice(index, 1);
   }
 }
 
